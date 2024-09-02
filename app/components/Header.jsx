0602c73a9a1a0003f41b17403e8a7c0220fdc8e4
Stan Yan <stanhavenoidea@gmail.com>
2024-09-02 22:25:34 +1000

    Drawer fixed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,8 @@
 "use client";
 
-// import nextjs tools
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-// import react icon
 import { RiMenu3Fill } from "react-icons/ri";
-// import shadcn
 import {
   Drawer,
   DrawerClose,
@@ -16,11 +13,15 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-// import VisuallyHidden for accessibility
 import { VisuallyHidden } from "@reach/visually-hidden";
+import { useState } from "react"; // Import useState
 
 const Header = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false); // State to manage Drawer
+
+  const toggleDrawer = () => setIsOpen(!isOpen);
+  const closeDrawer = () => setIsOpen(false); // Function to close Drawer
 
   return (
     <div className="fixed top-0 left-0 flex w-[100vw] h-[90px] md:h-[150px] text-white z-50">
@@ -30,8 +31,11 @@ const Header = () => {
         <p className="text-xs md:text-sm">JAPANESE DINING</p>
       </div>
       {/* menu icon */}
-      <Drawer>
-        <DrawerTrigger className="ml-auto pr-4 flex items-center">
+      <Drawer open={isOpen} onOpenChange={setIsOpen}>
+        <DrawerTrigger
+          className="ml-auto pr-4 flex items-center"
+          onClick={toggleDrawer}
+        >
           <RiMenu3Fill className="text-[30px] md:text-[50px]" />
         </DrawerTrigger>
         <DrawerContent
@@ -44,24 +48,28 @@ const Header = () => {
           <Link
             href="/"
             className={`nav_link ${pathname === "/" && "bg-[#94a3b8]"}`}
+            onClick={closeDrawer} // Close drawer on link click
           >
             主页 / HOME
           </Link>
           <Link
             href="/WhatsOn"
             className={`nav_link ${pathname === "/WhatsOn" && "bg-[#94a3b8]"}`}
+            onClick={closeDrawer} // Close drawer on link click
           >
             私たちについて / What&apos;s on
           </Link>
           <Link
             href="/Menu"
             className={`nav_link ${pathname === "/Menu" && "bg-[#94a3b8]"}`}
+            onClick={closeDrawer} // Close drawer on link click
           >
             メニュー / Menu
           </Link>
           <Link
             href="/Booking"
             className={`nav_link ${pathname === "/Booking" && "bg-[#94a3b8]"}`}
+            onClick={closeDrawer} // Close drawer on link click
           >
             予約 / Booking
           </Link>
