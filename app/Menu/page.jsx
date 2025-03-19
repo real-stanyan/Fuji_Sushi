d85887b26c9619c0c75d1bfcdd673bc3847e997f
Stan Yan <stanhavenoidea@gmail.com>
2025-03-19 14:35:09 +1100

    FriedDishes Done

--- app/Menu/page.jsx
+++ app/Menu/page.jsx
@@ -3,11 +3,12 @@ import Nigiri from "./pages/Nigiri";
 import Combo from "./pages/Combo";
 import FujiRolls from "./pages/FujiRolls";
 import Appetiser from "./pages/Appetiser";
+import FriedDishes from "./pages/FriedDishes";
 
 const Menu = () => {
   return (
     <div className="bg-menu-bg object-cover pt-[90px] md:pt-[120px] w-[100vw] min-h-[100vh]">
-      <Appetiser />
+      <FriedDishes />
     </div>
   );
 };
--- app/Menu/pages/FriedDishes.jsx
+++ app/Menu/pages/FriedDishes.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Image from "next/image";
 
 const FriedDishes = () => {
-  return <div>FriedDishes</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src="/menu_title/fried_dishes.webp"
+        width={1000}
+        height={1000}
+        className="w-[70vw] mb-[50px]"
+      />
+      {/* layer 1 */}
+      <Image
+        src="/menu_images/fried_dishes/layer_1.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[6vw] mb-[20px]"
+      />
+      <Image
+        src="/menu_images/hr.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[5vw] mb-[20px]"
+      />
+      {/* layer 2 */}
+      <Image
+        src="/menu_images/fried_dishes/layer_2.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[6vw] mb-[20px]"
+      />
+      <Image
+        src="/menu_images/hr.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[5vw] mb-[20px]"
+      />
+      {/* layer 3 */}
+      <Image
+        src="/menu_images/fried_dishes/layer_3.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[6vw] mb-[20px]"
+      />
+    </div>
+  );
 };
 
 export default FriedDishes;
