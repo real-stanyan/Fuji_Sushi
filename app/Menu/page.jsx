add4300de651633d26d51ad6922150a9debaedc8
Stan Yan <stanhavenoidea@gmail.com>
2025-03-19 15:13:24 +1100

    noodle

--- app/Menu/page.jsx
+++ app/Menu/page.jsx
@@ -6,11 +6,12 @@ import Appetiser from "./pages/Appetiser";
 import FriedDishes from "./pages/FriedDishes";
 import Salad from "./pages/Salad";
 import MainDishes from "./pages/MainDishes";
+import Noodle from "./pages/Noodle";
 
 const Menu = () => {
   return (
     <div className="bg-menu-bg object-cover pt-[90px] md:pt-[120px] w-[100vw] min-h-[100vh]">
-      <MainDishes />
+      <Noodle />
     </div>
   );
 };
--- app/Menu/pages/Noodle.jsx
+++ app/Menu/pages/Noodle.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
 const Noodle = () => {
-  return <div>Noodle</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src="/menu_title/noodle.webp"
+        width={1000}
+        height={1000}
+        className="w-[70vw] mb-[40px] ml-[10px]"
+      />
+      {/* layer 1 */}
+      <Image
+        src="/menu_images/noodle/layer_1.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[10vw] mb-[30px]"
+      />
+      {/* layer 2 */}
+      <Image
+        src="/menu_images/noodle/layer_2.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[10vw] mb-[30px]"
+      />
+      {/* layer 3 */}
+      <Image
+        src="/menu_images/noodle/layer_3.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[10vw] mb-[30px]"
+      />
+      {/* layer 4 */}
+      <Image
+        src="/menu_images/noodle/layer_4.webp"
+        width={1000}
+        height={1000}
+        className="w-[100vw] px-[10vw] mb-[30px]"
+      />
+    </div>
+  );
 };
 
 export default Noodle;
