1f8b43a8b38981d68a9fb0753e353fe147fd3bcb
Stan Yan <stanhavenoidea@gmail.com>
2025-04-20 17:15:33 +1000

    add BookingFull

---
+++ app/BookingFull/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="font-bold flex justify-center items-center  text-[white] bg-black w-[100vw] min-h-[100vh] pt-[90px] md:pt-[120px] space-y-4 pb-[40px] text-4xl whitespace-nowrap">
+      Fully Booked Today
+    </div>
+  );
+};
+
+export default page;
