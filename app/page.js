4ef2a149be632d7776e817219876f7284bb21714
Stan Yan <stanhavenoidea@gmail.com>
2025-09-04 10:59:16 +1000

    add Order

--- app/page.js
+++ app/page.js
@@ -17,14 +17,23 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="bg-home-bg bg-cover object-left-top min-w-[100vw] min-h-[100vh] flex justify-center items-center overflow-x-hidden">
-      <div className="font-pwc z-10 flex flex-col justify-evenly items-center text-white text-2xl md:text-4xl min-w-[85vw] min-h-[200px] md:min-w-[60vw] md:min-h-[400px] font-bold bg-black bg-opacity-50 rounded-xl p-4 ">
-        {/* <Link href="/WhatsOn">私たちについて / What&apos;s on</Link> */}
+    <div
+      className={`
+        bg-[url('/bgImage/home_bg.webp')] lg:bg-[url('/bgImages/home_bg_PC.webp')] bg-cover object-left-top min-w-[100vw] min-h-[100vh] flex justify-center items-center overflow-x-hidden
+        `}
+    >
+      <div
+        className={`
+          font-pwc z-10 flex flex-col justify-evenly items-center text-white text-2xl md:text-4xl min-w-[85vw] min-h-[200px] md:min-w-[60vw] 
+          md:min-h-[400px] font-bold bg-black bg-opacity-50 rounded-xl p-4
+          `}
+      >
         <Link href="/Menu" className="flex justify-center items-center gap-2">
           メニュー / Menu
           <Image src="/menu_icons/new.webp" width={40} height={40} />
         </Link>
         <Link href="/Booking">予約 / Booking</Link>
+        <Link href="https://bopple.app/fuji-sushi">ちゅうもん / Order</Link>
       </div>
     </div>
   );
