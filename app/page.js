564dd2b2fdc9892a755270f7a702f807eca6a3f7
Stan Yan <stanhavenoidea@gmail.com>
2025-10-13 23:24:25 +1100

    BookingFull

--- app/page.js
+++ app/page.js
@@ -108,7 +108,7 @@ export default function Home() {
           </Link>
 
           <Link
-            href="/Booking"
+            href="/BookingFull"
             className={`
               inline-flex items-center justify-center
               rounded-xl px-4 py-3
