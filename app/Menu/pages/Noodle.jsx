d1dcdcda554abcf16deaf42696e4eb0a047e53fe
Stan Yan <stanhavenoidea@gmail.com>
2025-11-07 17:28:58 +1100

    update picture

--- app/Menu/pages/Noodle.jsx
+++ app/Menu/pages/Noodle.jsx
@@ -38,6 +38,57 @@ const Noodle = () => {
         height={1000}
         className="w-[100vw] px-[10vw] mb-[30px]"
       />
+      {/* undo */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <h3
+          className={`
+              w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl
+               border-b-4 border-[#c42c24]
+                `}
+        >
+          Undo
+        </h3>
+        <Image
+          src="/menu_images/noodle/yakisoba.webp"
+          width={1000}
+          height={1000}
+          className="w-[30vw] flex-1 rounded-full"
+        />
+      </div>
+      {/* yakisoba */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <Image
+          src="/menu_images/noodle/undo.webp"
+          width={1000}
+          height={1000}
+          className="w-[30vw] flex-1 rounded-full"
+        />
+        <h3
+          className={`
+              w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl
+               border-b-4 border-[#c42c24]
+                `}
+        >
+          Yakisoba
+        </h3>
+      </div>
+      {/* curry */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <h3
+          className={`
+              w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl
+               border-b-4 border-[#c42c24]
+                `}
+        >
+          Curry
+        </h3>
+        <Image
+          src="/menu_images/noodle/curry.webp"
+          width={1000}
+          height={1000}
+          className="w-[30vw] flex-1 rounded-full"
+        />
+      </div>
     </div>
   );
 };
--- app/Menu/pages/Salad.jsx
+++ app/Menu/pages/Salad.jsx
@@ -38,6 +38,73 @@ const Salad = () => {
         height={1000}
         className="w-[100vw] px-[4vw] mb-[30px]"
       />
+      {/* chicken_katsu_and_rice */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <Image
+          src="/menu_images/salad/chicken_katsu_and_rice.webp"
+          width={1000}
+          height={1000}
+          className="w-[20vw] flex-1 rounded-full"
+        />
+        <h3
+          className={`
+        w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl border-b-4 border-[#c42c24]
+          `}
+        >
+          Chicken Katsu and Rice
+        </h3>
+      </div>
+      {/* kaarage_salad */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <h3
+          className={`
+        w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl
+         border-b-4 border-[#c42c24]
+          `}
+        >
+          Kaarage Salad
+        </h3>
+        <Image
+          src="/menu_images/salad/kaarage_salad.webp"
+          width={1000}
+          height={1000}
+          className="w-[20vw] flex-1 rounded-full"
+        />
+      </div>
+      {/* katsu_salad */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <Image
+          src="/menu_images/salad/katsu_salad.webp"
+          width={1000}
+          height={1000}
+          className="w-[20vw] flex-1 rounded-full"
+        />
+        <h3
+          className={`
+        w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl 
+        border-b-4 border-[#c42c24]
+          `}
+        >
+          Katsu Salad
+        </h3>
+      </div>
+      {/* Salmon Avocado Salad */}
+      <div className="flex w-[100vw] px-8 justify-between items-center my-3">
+        <h3
+          className={`
+        w-[70vw] text-center font-BebasNeue tracking-wider text-2xl lg:text-4xl xl:text-6xl 
+        border-b-4 border-[#c42c24]
+          `}
+        >
+          Salmon Avocado Salad
+        </h3>
+        <Image
+          src="/menu_images/salad/salmon_avo_salad.webp"
+          width={1000}
+          height={1000}
+          className="h-[20vw] flex-1 rounded-full"
+        />
+      </div>
     </div>
   );
 };
